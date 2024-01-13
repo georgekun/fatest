83040f65cb1c3f33f5c157ed27534656580c4186
jordan <jorj.knyazyan.15@gmail.com>
2024-01-13 20:21:02 +0300

    создание суперпользователя во время инициализации приложения

--- backend/notification_server/main/apps.py
+++ backend/notification_server/main/apps.py
@@ -7,3 +7,14 @@ class MainConfig(AppConfig):
 
     def ready(self) -> None:
         from . import receivers
+
+        from django.contrib.auth.models import User
+        from django.db.utils import IntegrityError
+
+        # Создание суперпользователя
+        try:
+            User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+            print('Superuser created successfully!')
+        except IntegrityError:
+            print('Superuser already exists.')
+
