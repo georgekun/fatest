c3a5692cc5ccb92c193cb2a81f0d0e947eede96b
jordan <jorj.knyazyan.15@gmail.com>
2024-01-13 20:19:49 +0300

    убрал из админки модели django-celery-beat, чтобы напрямую с ними не взаимодействовать

--- backend/notification_server/main/admin.py
+++ backend/notification_server/main/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 
+from django_celery_beat.models import (
+    IntervalSchedule,
+    CrontabSchedule,
+    SolarSchedule,
+    ClockedSchedule,
+    PeriodicTask,
+)
+
+
 from .models import Client, Newsletter, Message, Statistic
 
 admin.site.register([Client, Newsletter, Message, Statistic])
+
+admin.site.unregister([IntervalSchedule, CrontabSchedule, SolarSchedule, ClockedSchedule, PeriodicTask])
