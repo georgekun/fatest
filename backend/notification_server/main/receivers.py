0ac62538900bb10fe82710c2acd5cf4ec28aa8e9
jordan <jorj.knyazyan.15@gmail.com>
2024-01-13 15:57:22 +0300

    сигнал для обновление модели periodic task celery в зависимости от модели рассылок

--- backend/notification_server/main/receivers.py
+++ backend/notification_server/main/receivers.py
@@ -3,14 +3,15 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 
 from .models import Newsletter
-from .services.celery.services import create_task, update_task
+from .services.views.services import create_connect_task, update_connect_task
 
 
 @receiver(post_save, sender=Newsletter)
-def after_create_newsletter(sender, created, instance, **kwargs):
+def after_save_newsletter(sender, created, instance, **kwargs):
     if instance:
         if created:
-            create_task(instance)
+            create_connect_task(instance)
         else:
-            update_task(instance)
+            update_connect_task(instance)
+
 
