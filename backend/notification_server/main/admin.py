1c81c37778512ce3adbbf047d20a76a6603d3186
jordan <jorj.knyazyan.15@gmail.com>
2024-01-16 13:05:59 +0300

    убрал показ моделей из адмикни

--- backend/notification_server/main/admin.py
+++ backend/notification_server/main/admin.py
@@ -2,15 +2,14 @@ from django.contrib import admin
 
 from django_celery_beat.models import (
     IntervalSchedule,
-    CrontabSchedule,
     SolarSchedule,
     ClockedSchedule,
     PeriodicTask,
+    CrontabSchedule
 )
 
 
 from .models import Client, Newsletter, Message, Statistic
 
 admin.site.register([Client, Newsletter, Message, Statistic])
-
-admin.site.unregister([IntervalSchedule, CrontabSchedule, SolarSchedule, ClockedSchedule, PeriodicTask])
+admin.site.unregister([IntervalSchedule, SolarSchedule,CrontabSchedule, ClockedSchedule, PeriodicTask])
