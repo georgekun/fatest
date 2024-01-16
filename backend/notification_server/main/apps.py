38a10aaa1c3711ee4dd8129cd0deabaeb6cef065
jordan <jorj.knyazyan.15@gmail.com>
2024-01-16 13:06:59 +0300

    поправил конфиги. Так как в джанге функция ready вызывается и во время миграции

--- backend/notification_server/main/apps.py
+++ backend/notification_server/main/apps.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from django.apps import AppConfig
 
 
@@ -7,14 +9,25 @@ class MainConfig(AppConfig):
 
     def ready(self) -> None:
         from . import receivers
-
         from django.contrib.auth.models import User
         from django.db.utils import IntegrityError
+        from .services.celery.services import create_task_for_mailing
 
-        # Создание суперпользователя
-        try:
-            User.objects.create_superuser('admin', 'admin@example.com', 'admin')
-            print('Superuser created successfully!')
-        except IntegrityError:
-            print('Superuser already exists.')
+        if 'migrate' not in sys.argv:
+
+            # Создание суперпользователя
+            try:
+                User.objects.create_superuser(
+                    os.environ.get("ADMIN_USERNAME", "admin"),
+                    os.environ.get("ADMIN_EMAIL", 'admin@example.com'),
+                    os.environ.get("ADMIN_PASSWORD", "admin"))
+                print('Superuser created successfully!')
+            except IntegrityError:
+                print('Superuser already exists.')
+
+            # создание расписания для отправки отчета на почту
+            try:
+                create_task_for_mailing()
+            except IntegrityError:
+                print("task already exists")
 
