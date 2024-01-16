df2fd4451f96bad8c60461f8f2a71ef44a6b6c2d
jordan <jorj.knyazyan.15@gmail.com>
2024-01-16 13:11:14 +0300

    пути для файлов

--- backend/notification_server/notification_server/urls.py
+++ backend/notification_server/notification_server/urls.py
@@ -40,5 +40,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include(urls)),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
