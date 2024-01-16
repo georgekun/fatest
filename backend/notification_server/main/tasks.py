177b3d132a61246da0699a3829e772ad88938fce
jordan <jorj.knyazyan.15@gmail.com>
2024-01-16 13:07:46 +0300

    task  для отправки отчета на почту админов

--- backend/notification_server/main/tasks.py
+++ backend/notification_server/main/tasks.py
@@ -1,8 +1,13 @@
 
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.utils import timezone
+
 from celery import shared_task
 
 from .models import Statistic
 from .services.celery.services import get_filtered_clients, create_messages, send_message
+from .services.mailing.utils import create_report_file, send_email_with_attachment
 
 
 @shared_task(name='send_newsletter')
@@ -21,4 +26,18 @@ def send_newsletter(**kwargs):
         message.save()
     return True
 
+
+@shared_task(name='send_report_by_email')
+def send_report_by_email() -> None:
+    file_path = create_report_file(f'file_{timezone.now()}')
+    admin_users = User.objects.filter(is_staff=True, is_superuser=True)
+    to_email = list(admin_users.values_list('email', flat=True))
+
+    return send_email_with_attachment(
+            subject="Report",
+            message="Отчет по рассылкам находятся в этом файле\n",
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=to_email,
+            attachment_path=file_path
+        )
 
